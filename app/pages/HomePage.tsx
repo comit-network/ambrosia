dfeec4e28402d9d7c9a963214c707c6565c3038d
Yos Riady <yos.riady@tenx.tech>
2020-06-11 14:13:03 +0800

    Update repo metadata

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import Home from '../components/Home';
+import { Link } from 'react-router-dom';
+import routes from '../constants/routes.json';
+import styles from './Home.css';
 
 export default function HomePage() {
-  return <Home />;
+  return (
+    <div className={styles.container} data-tid="container">
+      <h2>Home</h2>
+      <Link to={routes.COUNTER}>to Counter</Link>
+    </div>
+  );
 }
