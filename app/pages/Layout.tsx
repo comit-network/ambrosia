2bcaf4739baaa21cbce9f1dfdbe67f00871e68a6
Daniel Karzel <daniel@comit.network>
2020-08-13 17:15:53 +1000

    Somewhat nice navigation with Drawer based menu
    Currently only uses Dashboard and About page

--- app/pages/Layout.tsx
+++ app/pages/Layout.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route, Link } from 'react-router-dom';
-import {Box, Flex, PseudoBox, Icon, Stack, Text, Collapse, Button} from '@chakra-ui/core';
+import {
+  Box,
+  Flex,
+  PseudoBox,
+  Icon,
+  Stack,
+  Text,
+  Drawer,
+  useDisclosure,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  IconButton,
+  Divider
+} from '@chakra-ui/core';
 import styled from '@emotion/styled';
 import routes from '../constants/routes.json';
 import DashboardPage from './DashboardPage';
-import HistoryPage from './HistoryPage';
-import ExchangePage from './ExchangePage';
-import OrderConfirmationPage from './OrderConfirmationPage';
-import SwapDetailsPage from './SwapDetailsPage';
-import SettingsPage from './SettingsPage';
 import AboutPage from './AboutPage';
+import WalletPage from './WalletPage';
 
 const NavIcon = styled(Icon)`
   margin-top: -3px;
@@ -21,7 +32,7 @@ const NavText = styled(Text)`
   font-size: 0.9em;
   font-weight: 500;
 `;
-const NavBox = ({ children }) => {
+const MenuNavBox = ({ children }) => {
   return (
     <PseudoBox
       as={Box}
@@ -38,42 +49,107 @@ const NavBox = ({ children }) => {
   );
 };
 
+const DASHBOARD = 'DASHBOARD';
+const ABOUT = 'ABOUT';
+const WALLET = 'WALLET';
+
 export default function Layout() {
-  const [show, setShow] = React.useState(false);
-  const [sidebarWidth, setSidebarWidth] = React.useState(10);
-  const handleToggle = () => {
-    setShow(!show);
-    if (show === false) {
-      setSidebarWidth(200)
-    }
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [activeContent, setActiveContent] = useState(DASHBOARD);
+
+  const handleClick = () => {
+    onOpen();
   };
 
   return (
-    <Flex flexDirection="row" minHeight="100%" alignItems="stretch" marginTop={10}>
+    <Flex
+      flexDirection="row"
+      minHeight="100%"
+      alignItems="stretch"
+      marginTop={10}
+      marginBottom={10}
+    >
       <Flex backgroundColor="white" id="sidebar">
         <Stack>
-          <Button mt={4} w={10} variantColor="blue" onClick={handleToggle}>
-            M
-          </Button>
-          <Collapse mt={2} isOpen={show} w={sidebarWidth}>
-            <Stack spacing="20px">
-              {/*<Text fontSize="0.7em" mt={6} fontWeight="600">*/}
-              {/*  MENU*/}
-              {/*</Text>*/}
-              <Stack>
-                <Link to={routes.HOME}>
-                  <NavBox>
-                    <NavIcon name="calendar" />
-                    <NavText>Dashboard</NavText>
-                  </NavBox>
-                </Link>
-              </Stack>
-            </Stack>
-          </Collapse>
+          <IconButton
+            aria-label="Menu"
+            variantColor="gray"
+            onClick={handleClick}
+            icon="chevron-right"
+          />
+          <Divider />
+          <Stack>
+            <Link to={routes.HOME}>
+              <IconButton
+                aria-label="Dashboard"
+                variantColor={activeContent === DASHBOARD ? 'blue' : 'gray'}
+                icon="calendar"
+                onClick={() => setActiveContent(DASHBOARD)}
+                marginBottom="10px"
+              />
+            </Link>
+            <Link to={routes.ABOUT}>
+              <IconButton
+                aria-label="About"
+                variantColor={activeContent === ABOUT ? 'blue' : 'gray'}
+                icon="info"
+                onClick={() => setActiveContent(ABOUT)}
+                marginBottom="10px"
+              />
+            </Link>
+            <Link to={routes.WALLET}>
+              <IconButton
+                aria-label="Wallet"
+                variantColor={activeContent === WALLET ? 'blue' : 'gray'}
+                icon="star"
+                onClick={() => setActiveContent(WALLET)}
+                marginBottom="10px"
+              />
+            </Link>
+          </Stack>
+          <Drawer placement="left" onClose={onClose} isOpen={isOpen} size="xs">
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerHeader marginTop="20px">MENU</DrawerHeader>
+              <DrawerBody>
+                <Stack spacing="20px">
+                  <Link
+                    to={routes.HOME}
+                    onClick={() => setActiveContent(DASHBOARD)}
+                  >
+                    <MenuNavBox>
+                      <NavIcon name="calendar" />
+                      <NavText>Dashboard</NavText>
+                    </MenuNavBox>
+                  </Link>
+                  <Link
+                    to={routes.ABOUT}
+                    onClick={() => setActiveContent(ABOUT)}
+                  >
+                    <MenuNavBox>
+                      <NavIcon name="info" />
+                      <NavText>About</NavText>
+                    </MenuNavBox>
+                  </Link>
+                  <Link
+                    to={routes.WALLET}
+                    onClick={() => setActiveContent(WALLET)}
+                  >
+                    <MenuNavBox>
+                      <NavIcon name="star" />
+                      <NavText>Wallet</NavText>
+                    </MenuNavBox>
+                  </Link>
+                </Stack>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
         </Stack>
       </Flex>
       <Flex backgroundColor="gray.100" id="content">
         <Switch>
+          <Route path={routes.ABOUT} component={AboutPage} />
+          <Route path={routes.WALLET} component={WalletPage} />
           <Route path={routes.HOME} component={DashboardPage} />
         </Switch>
       </Flex>
@@ -81,6 +157,6 @@ export default function Layout() {
   );
 }
 
-NavBox.propTypes = {
+MenuNavBox.propTypes = {
   children: PropTypes.node.isRequired
 };
