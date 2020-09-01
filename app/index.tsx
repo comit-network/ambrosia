fb2859d612276a937cd25eca1ff09290ec666fcf
Thomas Eizinger <thomas@eizinger.io>
2020-09-01 15:42:35 +1000

    Automatically create Bitcoin wallet in dev mode

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,9 @@ import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 import { Config, fromComitEnv } from './config';
 import ElectronStore from 'electron-store';
+import { LedgerBitcoinWallet } from './hooks/useLedgerBitcoinWallet';
+import { LedgerClient } from './ledgerIpc';
+import { ipcRenderer } from "electron";
 
 const reduxStore = configureStore();
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
@@ -20,6 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
   // if comit-env is present during development, use that, otherwise load file system config
   const effectiveConfig = comitEnvConfig && process.env.NODE_ENV === 'development' ? comitEnvConfig : store;
 
+  if (comitEnvConfig && process.env.NODE_ENV === 'development') {
+    console.info("Running in development mode using comit-scripts, creating Bitcoin wallet ...");
+    importDescriptorOfDanielsLedger(comitEnvConfig);
+  }
+
   render(
     <AppContainer>
       <App store={reduxStore} history={history} settings={effectiveConfig} />
@@ -27,3 +35,16 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('root')
   );
 });
+
+/*
+ * Imports the descriptors of our testing Ledger into the bitcoind's wallet.
+ *
+ * Together with the `ComitScripts.toml` config file, this allows us to use the UI without having the Ledger set up on a particular machine.
+ */
+async function importDescriptorOfDanielsLedger(config: Config) {
+  const ledgerBitcoinWallet = new LedgerBitcoinWallet(new LedgerClient(ipcRenderer), config.LEDGER_BITCOIN_ACCOUNT_INDEX, config.BITCOIND_ENDPOINT);
+  await ledgerBitcoinWallet.createWallet({
+    internal: "wpkh([47036c27/84'/1'/0']tpubDDTJSQZQhRLcz2dTxYZRDCvj8SdHgYfkcxfkrq6rgPRNrXhxfYDmMrifgZS6qzxrsRqMQrsLjpwewfMZJbcRScV9MHy8RJvf83Vv3v4fn2g/0/*)#cyp75tc5",
+    external: "wpkh([47036c27/84'/1'/0']tpubDDTJSQZQhRLcz2dTxYZRDCvj8SdHgYfkcxfkrq6rgPRNrXhxfYDmMrifgZS6qzxrsRqMQrsLjpwewfMZJbcRScV9MHy8RJvf83Vv3v4fn2g/1/*)#fsylf7gv"
+  });
+}
