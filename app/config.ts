23845dde2b038eb217409e12cbbbba1e8046989f
Daniel Karzel <daniel@comit.network>
2020-10-07 10:43:23 +1100

    Update dev-env addresses due to derivation path changes

--- app/config.ts
+++ app/config.ts
@@ -39,20 +39,25 @@ export function fromComitEnv(): Config | null {
       break;
   }
 
-  return {
+  const config = {
     BITCOIND_ENDPOINT: `http://${comitEnv.BITCOIN_USERNAME}:${comitEnv.BITCOIN_PASSWORD}@${bitcoindHttpUri.host}`,
     WEB3_ENDPOINT: comitEnv.ETHEREUM_NODE_HTTP_URL,
     LEDGER_BITCOIN_ACCOUNT_INDEX: role === Role.ALICE ? 0 : 1,
     LEDGER_ETHEREUM_ACCOUNT_INDEX: role === Role.ALICE ? 0 : 1,
     LEDGER_ETHEREUM_ACCOUNT_ADDRESS:
       role === Role.ALICE
-        ? '0x5087fb5F19f8EF0585b4EFcb3375De97C9d0fE0e'
-        : '0xeD56BBA7d30FEb908aB7a5a7b3b16654cb45a38C',
+        ? '0xf205D55E96c607EEe898Cf20e515559fe0134317'
+        : '0x0C81b364E733E940A23669c5b11DF3f94cb542cA',
     CND_URL:
       role === Role.ALICE ? 'http://127.0.0.1:8000' : 'http://127.0.0.1:8100',
     SETUP_COMPLETE: true,
     ROLE: role
   };
+
+  console.info('Starting in dev-mode with config:');
+  console.info(config);
+
+  return config;
 }
 
 type ConfigSetter = (config: Partial<Config>) => void;
